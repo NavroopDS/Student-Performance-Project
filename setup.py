4f4f116d38a28f05dc70c873ab4ba7004ff3dde2
NavroopDS <navroopkaurphd@gmail.com>
2025-05-20 10:33:26 +0100

    touchup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='studentproj',
+name='mlproject',
 version='0.0.1',
 author='Navroop',
 author_email='navroopkaurphd@gmail.com',
